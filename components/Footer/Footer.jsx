69c6242be5589dc1f30987313e8c92ca756f4298
Steven Whitfield <stevmwhitfield@gmail.com>
2022-04-15 14:06:29 -0400

    Footer component with styles

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
-  return <footer>&copy; 2022 Steven Whitfield. All Rights Reserved.</footer>;
+  return (
+    <footer id={styles.footer}>
+      &copy; 2022 Steven Whitfield. All Rights Reserved.
+    </footer>
+  );
 };
 
 export default Footer;
