c0c687f502358b8b4a577c2ff9f55ee34d1eeb4a
Steven Whitfield <stevmwhitfield@gmail.com>
2022-04-22 09:15:20 -0400

    Fix invalid property name

--- pages/weather.jsx
+++ pages/weather.jsx
@@ -40,7 +40,7 @@ const WeatherPage = () => {
     <div id="wrapper">
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Lakeland Weather - Florida Poly Archery</title>
         <link rel="icon" type="image" href="/favicon.png" />
